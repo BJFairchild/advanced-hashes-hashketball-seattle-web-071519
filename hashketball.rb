2d5d3badaeda8cac7eb988cee5708c2de5eb3ff9
Brittan Fairchild <brittanfairchild@gmail.com>
2019-07-13 03:50:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -120,12 +120,8 @@ def game_hash
 end
 
 def num_points_scored(player_n)
-  game_hash.each do |home_away, keys|
-    keys[:players].each do |player|
-      return player[:points] if player[:player_name] == player_n
-    end
-  end
-end
+   player_stats(player_n)[:points]
+ end
 
 
  def shoe_size(player_name)
